27a6b27094b494ed9b50efbf7fe8192fb7d89373
msdeek <msetiha@gmail.com>
2022-08-13 19:31:27 +0200

    wp_localize_script

--- admin/class-cpanel-admin.php
+++ admin/class-cpanel-admin.php
@@ -109,8 +109,10 @@ class cpanel_Admin {
 		$token =  get_option( 'cptoken');
 		#var_dump($token);
 		$content = new CPanel_Content;
-		$response['courses'] = $content->get_cpanel_courses($baseurl, $token);
-		$response['lessons'] = $content->get_cpanel_items($baseurl, $token);
+		#$response['courses'] = $content->get_cpanel_courses($baseurl, $token);
+		#$response['lessons'] = $content->get_cpanel_items($baseurl, $token);
+		$courses = $content->get_cpanel_courses($baseurl, $token);
+		$lessons = $content->get_cpanel_items($baseurl, $token);
 		#$users = new CPanelUsers;
 		#$users_data = $users->core_user_create_users();
 		#$response = json_encode( $response);
